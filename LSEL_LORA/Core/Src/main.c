59b6c3b49e5c83f9d82b91747ac08999315f4043
JavierVillarPerez <javier.villar.perez@alumnos.upm.es>
2020-05-09 13:17:35 +0200

    USB interface for LoRa GW

--- LSEL_LORA/Core/Src/main.c
+++ LSEL_LORA/Core/Src/main.c
@@ -1,81 +1,170 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "spi.h"
+#include "usb_device.h"
 #include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "usbd_cdc_if.h"
+#include <string.h>
 #include "stdio.h"
 #include "SX1278.h"
+/* USER CODE END Includes */
 
-int _write(int file, char *ptr, int len) {
-	int i;
-	for (i = 0; i < len; i++) {
-		ITM_SendChar(*ptr++);
-	}
-	return len;
-}
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
 
-void SystemClock_Config(void);
+/* USER CODE END PTD */
 
-void Lora_inicio(int init);
-void Lora_recibe(void);
-void Lora_envia(sensor_buf_t prueba);
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
 
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
 SX1278_hw_t SX1278_hw;
 SX1278_t SX1278;
 
 int master=0;
 
-//Struct creado en main.h
-sensor_buf_t prueba;
+/* USER CODE END PV */
 
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+void Lora_inicio(int init);
+void Lora_recibe(void);
+void Lora_envia(sensor_buf_t prueba);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
-	HAL_Init();
-	SystemClock_Config();
-	MX_GPIO_Init();
-	MX_SPI1_Init();
-	Lora_inicio(master);  //0 es esclavo, 1 es maestro
+  /* USER CODE BEGIN 1 */
+	uint8_t buffer[] = "Hello world!\r\n";
+	uint8_t len = 0;
+	sensor_buf_t prueba;
 
-	prueba.ID = 1;
-	prueba.measure = 100;
-	prueba.alarm = 0;
-	prueba.error = 0;
-	prueba.threshold_L = 80;
-	prueba.threshold_H = 120;
+  /* USER CODE END 1 */
 
-	  while (1)
-	  {
-			if (master == 1) {
-				Lora_envia(prueba);
-				HAL_Delay(1000);
-			} else {
-				Lora_recibe();
-			}
-	  }
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  MX_USB_DEVICE_Init();
+  /* USER CODE BEGIN 2 */
+  len = strlen((char*) buffer);
+
+  Lora_inicio(master);  //0 es esclavo, 1 es maestro
+
+  prueba.ID = 1;
+  prueba.measure = 100;
+  prueba.alarm = 0;
+  prueba.error = 0;
+  prueba.threshold_L = 80;
+  prueba.threshold_H = 120;
+
+  HAL_Delay(1000);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	if (master == 1) {
+		Lora_envia(prueba);
+		HAL_Delay(1000);
+	} else {
+		Lora_recibe();
+	}
+	  CDC_Transmit_FS(buffer, len);
+	  HAL_Delay(1000);
+  }
+  /* USER CODE END 3 */
 }
 
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
+  /** Configure the main internal regulator output voltage 
   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-  /** Initializes the CPU, AHB and APB busses clocks
+  /** Initializes the CPU, AHB and APB busses clocks 
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 144;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 72;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 8;
+  RCC_OscInitStruct.PLL.PLLQ = 3;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
   }
-  /** Initializes the CPU, AHB and APB busses clocks
+  /** Initializes the CPU, AHB and APB busses clocks 
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
@@ -91,6 +180,14 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+int _write(int file, char *ptr, int len) {
+	int i;
+	for (i = 0; i < len; i++) {
+		ITM_SendChar(*ptr++);
+	}
+	return len;
+}
+
 void Lora_inicio(int init){
 	int ret;
 	//initialize LoRa module
